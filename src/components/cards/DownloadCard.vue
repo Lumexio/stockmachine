b8d07d94f7ed5f5670ee465ea22f5e51ac473434
Lumexio <trotroyu@gmail.com>
2024-09-19 20:50:44 -0600

    Refactor file paths and names for improved organization and consistency

--- src/components/cards/DownloadCard.vue
+++ src/components/cards/DownloadCard.vue
@@ -6,20 +6,20 @@
     <div class='card-actions'>
       <button class='button-accent' @click="handleDownload('windows')">
         <FontAwesomeIcon size='2x' :icon='faDownload' />
-        <h3>Windows 1.0.0 version </h3>
+        <h3>Windows 1.0.0 version</h3>
       </button>
-      <button class="button-accent" @click="handleDownload(' linux')">
+      <button class="button-accent" @click="handleDownload('linux')">
         <FontAwesomeIcon size='2x' :icon='faDownload' />
         <h3>Linux 1.0.0 version</h3>
       </button>
     </div>
   </div>
 </template>
+
 <script setup>
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
-let path = '../../todownload/'
-let fileName = ''
+
 function downloadFile(url, filename) {
   const link = document.createElement('a');
   link.href = url;
@@ -30,12 +30,19 @@ function downloadFile(url, filename) {
 }
 
 function handleDownload(os) {
-  if (os == 'windows') {
+  let path = '/todownload/';
+  let fileName = '';
+
+  if (os === 'windows') {
     fileName = 'STOCKMACHINE-1.0.0 Setup.exe';
-    downloadFile(path, fileName);
   } else {
     fileName = 'stockmachine_1.0.0_amd64.deb';
-    downloadFile(path, fileName);
   }
+
+  downloadFile(`${path}${fileName}`, fileName);
 }
 </script>
+
+<style>
+/* Add your styles here */
+</style>
